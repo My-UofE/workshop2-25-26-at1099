6ebad84e77b6530896c047f1c61d2b2004d4e71b
at1099 <at1099@exeter.ac.uk>
2026-01-28 14:45:06 +0000

    First Part

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -1,3 +1,6 @@
-// Add your code below
-
-
+// HelloWorld application
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World! My name is " + args[0] + " I am "+ args[1] + "years old.");
+    }
+}
